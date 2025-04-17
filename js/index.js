3b0a7e02e06e442a8bf78bf90540235e002029fd
julia rs <julia.kaova@gmail.com>
2025-04-18 01:07:27 +0200

    works sections styles

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const topFingers = document.querySelectorAll(".top");
 const bottomFingers = document.querySelectorAll(".bottom");
 const shutter= document.querySelector(".shutter");
 
+
 // empiezo con este setTimeOut para empezar la animación de los fingers, como el intervalo no comienza hasta los 2s y quiero que empiece en 1s, de esta manera me permite comezar antes a nivel visual la animación
 topFingers.forEach((topFinger) => {
     setTimeout(() => {
@@ -44,4 +45,36 @@ setInterval(()=>{
     setTimeout(()=>{
         shutter.classList.remove("click");
     }, 1000);
-}, 8000);
+}, 6000);
+
+//curvar tittle2
+const circleTop= document.querySelector(".circleTop");
+const circleTopArray = circleTop.innerText.split("");
+let letters= "";
+
+for(let i= 0; i<circleTopArray.length; i++){
+    letters += `<span style= "transform: rotate(${i+1*10}deg);display: inline-block">${circleTopArray[i]}</span>`;
+}
+circleTop.innerHTML= letters;
+
+//dar visibilidad a los navs, a traves de clases, de esta manera solo puede estar visible uno de los navs del landing, nunca los dos a la vez y además pueden cerrarse de nuevo los navs haciendo click
+const dosD = document.querySelector(".camera");
+const navs = document.querySelectorAll("nav");
+const tresD = document.querySelector(".hands");
+
+
+dosD.addEventListener("click", () => {
+    navs[0].classList.add("options2D");
+    navs[1].classList.remove("options3D");
+});
+
+tresD.addEventListener("click", () => {
+    navs[1].classList.add("options3D");
+    navs[0].classList.remove("options2D");
+});
+
+navs.forEach((nav)=>{
+    nav.addEventListener("click", () => {
+        nav.className="";
+    });
+});
